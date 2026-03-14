7ab5c83cae0c98dea617512dc95d8681a647e9d4
Balaji <Balajisenthilkannan@gmail.com>
2026-03-14 15:46:06 +0530

    Update on scroller and text speed in the home page

--- script.js
+++ script.js
@@ -160,11 +160,11 @@
       heroInspiresEl.textContent = "";
 
       heroLine1El.classList.add("hero__line--visible");
-      await typeText(heroLine1El, "We Create Content that", 80);
+      await typeText(heroLine1El, "We Create Content that", 50);
 
       heroLine2El.classList.add("hero__line--visible");
-      await typeText(heroConnectsEl, "Connects", 80);
-      await typeText(heroInspiresEl, "Inspires", 80);
+      await typeText(heroConnectsEl, "Connects", 50);
+      await typeText(heroInspiresEl, "Inspires", 50);
     };
 
     runHeroTyping();
@@ -178,42 +178,42 @@ const ctx = canvas.getContext('2d');
 let particles = [];
 
 function initCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
 class Particle {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 2;
-        this.speedX = Math.random() * 1 - 0.5;
-        this.speedY = Math.random() * 1 - 0.5;
-        this.color = Math.random() > 0.5 ? '#F27D26' : '#00F2FF';
-    }
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if (this.x > canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
-    }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 2;
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = Math.random() * 1 - 0.5;
+    this.color = Math.random() > 0.5 ? '#F27D26' : '#00F2FF';
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.x > canvas.width) this.x = 0;
+    if (this.x < 0) this.x = canvas.width;
+    if (this.y > canvas.height) this.y = 0;
+    if (this.y < 0) this.y = canvas.height;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles.forEach(p => { p.update(); p.draw(); });
-    requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(p => { p.update(); p.draw(); });
+  requestAnimationFrame(animate);
 }
 
 window.addEventListener('resize', initCanvas);
 initCanvas();
-for(let i=0; i<50; i++) particles.push(new Particle());
+for (let i = 0; i < 50; i++) particles.push(new Particle());
 animate();
